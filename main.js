7552d71ddbfa7b02481c6165cbd2ab0744125554
sergedemanou <57003109+sergedemanou@users.noreply.github.com>
2020-11-07 15:30:32 +0100

    updates

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-fetch("https://pomber.github.io/covid19/timeseries.json")
-  .then(response => response.json())
-  .then(data => {
-    data["Argentina"].forEach(({ date, confirmed, recovered, deaths }) =>
-      console.log(`${date} active cases: ${confirmed - recovered - deaths}`)
-    );
-  });
+
+let myForm = document.getElementById('myForm');
+    let submit = document.getElementById("submit");
+    submit.addEventListener('click', f_valid);
+
+myForm.addEventListener = ('submit', function(e){
+      let error;
+      let inputName = document.getElementById('inputName')
+      let inputEmail = document.getElementById('inputEmail')
+      let inputMessage = document.getElementById('inputMessage')
+      let missing_e = document.getElementById('missing_e');
+
+   let inputs = document.getElementsByTagName('input');
+
+   for (let i=0; i<inputs.length; i++){
+       if(!inputs[i].value){
+           error = "Please fill in all the Fields*"
+        }
+       else{
+           alert('Message sent! Thank You')
+       }
+   }
+       if(error){
+           e.preventDefault();
+           document.getElementById("error").innerHTML= error;
+           return false;
+       }
+       else{
+           alert('Mesaage sent! Thank You')
+       }
+})
+
